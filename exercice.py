fbb3591986f1180677d2d64b90277f1e9861a88b
nayarramo <nayarramo@outlook.com>
2022-10-07 00:29:40 -0400

    1

--- exercice.py
+++ exercice.py
@@ -4,14 +4,20 @@
 import random
 
 def get_bill(name, data):
-	INDEX_NAME = 0
-	INDEX_QUANTITY = 1
-	INDEX_PRICE = 2
+	sous_total = data[0][1]*data[0][2] + data[1][1]*data[1][2]
+	taxes = sous_total *0.15
+	total = sous_total + taxes
 
-	return ""
+	result = name
+	bill_lines = [("SOUS TOTAL", sous_total), ("TAXES     ", taxes), ("TOTAL     ", total)]
+	for line in bill_lines:
+		result += "\n" + f"{line[0]} {line[1]: 10.2f} $"
+
+	return result
 
 
 def format_number(number, num_decimal_digits):
+	
 	return ""
 
 def get_triangle(num_rows):
